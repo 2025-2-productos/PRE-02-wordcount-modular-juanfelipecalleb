d1f06b2e0e94fc9e70d5bf590ac31fa1d986bcca
juanfelipecalleb <jcalleb@unal.edu.co>
2025-11-22 11:32:49 -0500

    homework

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,27 +1,26 @@
-# obtain a list of files in the input directory
+import sys
 
 from ._internals.count_words import count_words
 from ._internals.preprocess_lines import preprocess_lines
 from ._internals.read_all_lines import read_all_lines
 from ._internals.split_into_words import split_into_words
-from ._internals.write_count_words import write_count_words
+from ._internals.write_word_counts import write_count_words
 
 
 def main():
-    input_folder = "data/input/"
-    output_folder = "data/output/"
-    # mover a la funcion read_all_lines
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        # sys.exit(1)
+        return
+
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
     all_lines = read_all_lines(input_folder)
-
-    # mover a la funcion preprocess_lines
     all_lines = preprocess_lines(all_lines)
-
-    # mover a la funcion split in words
     words = split_into_words(all_lines)
-
-    # mover a la funcion count words
     counter = count_words(words)
-
     write_count_words(counter, output_folder)
 
 
